c2392801000fcc4cb6d8c13b4fcec457eb95b809
Rishabh Srivastava <rishabhsriv@gmail.com>
2024-11-11 22:25:53 +0800

    add support for o1 models when calling llms

--- defog_utils/utils_multi_llm.py
+++ defog_utils/utils_multi_llm.py
@@ -1,7 +1,14 @@
 import concurrent
 from typing import Callable, Dict
 
-from .utils_llm import LLMResponse, chat_anthropic, chat_gemini, chat_openai, chat_together
+from .utils_llm import (
+    LLMResponse,
+    chat_anthropic,
+    chat_gemini,
+    chat_openai,
+    chat_together,
+)
+
 
 def map_model_to_chat_fn(model: str) -> Callable:
     """
@@ -11,18 +18,40 @@ def map_model_to_chat_fn(model: str) -> Callable:
         return chat_anthropic
     if model.startswith("gemini"):
         return chat_gemini
-    if model.startswith("gpt"):
+    if model.startswith("gpt") or model in ["o1", "o1-mini", "o1-preview"]:
         return chat_openai
-    if model.startswith("meta-llama") or model.startswith("mistralai") or model.startswith("Qwen"):
+    if (
+        model.startswith("meta-llama")
+        or model.startswith("mistralai")
+        or model.startswith("Qwen")
+    ):
         return chat_together
     raise ValueError(f"Unknown model: {model}")
 
-def chat(models, messages, max_tokens=8192, temperature=0.0, stop=[], json_mode=False, seed=0) -> Dict[str, LLMResponse]:
+
+def chat(
+    models, messages, max_tokens=8192, temperature=0.0, stop=[], json_mode=False, seed=0
+) -> Dict[str, LLMResponse]:
     """
     Returns the response from the LLM API for each of the models passed in.
     Output format is a dictionary keyed by model name.
     """
     with concurrent.futures.ThreadPoolExecutor(max_workers=len(models)) as executor:
-        futures = {executor.submit(map_model_to_chat_fn(model), messages, model, max_tokens, temperature, stop, json_mode, seed): model for model in models}
-        responses = {futures[future]: future.result() for future in concurrent.futures.as_completed(futures)}
+        futures = {
+            executor.submit(
+                map_model_to_chat_fn(model),
+                messages,
+                model,
+                max_tokens,
+                temperature,
+                stop,
+                json_mode,
+                seed,
+            ): model
+            for model in models
+        }
+        responses = {
+            futures[future]: future.result()
+            for future in concurrent.futures.as_completed(futures)
+        }
     return responses
