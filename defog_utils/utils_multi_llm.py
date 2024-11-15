760bc686415dda25e7b8392ed1a45d9f44eef8ed
Rishabh Srivastava <rishabhsriv@gmail.com>
2024-11-15 14:32:12 +0800

    add asynchronous chat function for llm calls

--- defog_utils/utils_multi_llm.py
+++ defog_utils/utils_multi_llm.py
@@ -7,6 +7,9 @@ from .utils_llm import (
     chat_gemini,
     chat_openai,
     chat_together,
+    chat_anthropic_async,
+    chat_openai_async,
+    chat_together_async,
 )
 
 
@@ -27,6 +30,48 @@ def map_model_to_chat_fn(model: str) -> Callable:
     ):
         return chat_together
     raise ValueError(f"Unknown model: {model}")
+
+
+def map_model_to_chat_fn_async(model: str) -> Callable:
+    """
+    Returns the appropriate chat function based on the model.
+    """
+    if model.startswith("claude"):
+        return chat_anthropic_async
+    if model.startswith("gemini"):
+        return ValueError("Gemini does not support async chat")
+    if model.startswith("gpt") or model in ["o1", "o1-mini", "o1-preview"]:
+        return chat_openai_async
+    if (
+        model.startswith("meta-llama")
+        or model.startswith("mistralai")
+        or model.startswith("Qwen")
+    ):
+        return chat_together_async
+    raise ValueError(f"Unknown model: {model}")
+
+
+async def chat_async(
+    model,
+    messages,
+    max_completion_tokens=4096,
+    temperature=0.0,
+    stop=[],
+    json_mode=False,
+    seed=0,
+) -> LLMResponse:
+    """
+    Returns the response from the LLM API for a single model that is passed in.
+    """
+    llm_function = map_model_to_chat_fn_async(model)
+    return await llm_function(
+        messages=messages,
+        max_completion_tokens=max_completion_tokens,
+        temperature=temperature,
+        stop=stop,
+        json_mode=json_mode,
+        seed=seed,
+    )
 
 
 def chat(
