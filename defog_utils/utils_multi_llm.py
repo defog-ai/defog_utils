e7a5096789d49ae1e2b7c071d4fed012284d0362
Rishabh Srivastava <rishabhsriv@gmail.com>
2025-02-01 14:38:30 +0800

    Update utils_multi_llm.py

--- defog_utils/utils_multi_llm.py
+++ defog_utils/utils_multi_llm.py
@@ -45,7 +45,7 @@ def map_model_to_chat_fn_async(model: str) -> Callable:
         return chat_anthropic_async
     if model.startswith("gemini"):
         return chat_gemini_async
-    if model.startswith("gpt") or model.startswith("o1") or model.startswith("chatgpt"):
+    if model.startswith("gpt") or model.startswith("o1") or model.startswith("chatgpt") or model.startswith("o3"):
         return chat_openai_async
     if model.startswith("deepseek"):
         return chat_openai_async
